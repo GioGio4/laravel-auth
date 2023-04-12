d30fbe3cebb19674845984fa9c2b7e4eebfc9cce
GioGio4 <g.ceccobelli@outlook.it>
2023-04-12 20:02:17 +0200

    Create show style

--- resources/views/Admin/projects/show.blade.php
+++ resources/views/Admin/projects/show.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    @dump($project)
+    <h3 class="my-3 text-center">{{ $project->title }}</h3>
+    <div class="card my-3">
+        <div class="row g-0">
+            <div class="col-md-12">
+                <div class="card-body">
+                    <p class="card-text"><strong>ID - </strong> {{ $project->id }} </p>
+                    <p class="card-text"><strong>Title - </strong> {{ $project->title }} </p>
+                    <p class="card-text"><strong>Description - </strong> {{ $project->description }}</p>
+                    <p class="card-text"><strong>Languages - </strong> {{ $project->languages }}</p>
+                    <p class="card-text"><strong>Project link - </strong> {{ $project->link }}</p>
+                    <p class="card-text"><strong>Image url - </strong><a href="{{ $project->pic }}">{{ $project->pic }}</a>
+                    </p>
+                    <p class="card-text"><strong>Create - </strong> {{ $project->created_at }}</p>
+                    <p class="card-text"><strong>Last update - </strong> {{ $project->updated_at }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Return to projects</a>
 @endsection
--- resources/views/Guest/home.blade.php
+++ resources/views/Guest/home.blade.php
@@ -4,9 +4,11 @@
         Portfolio "nome"
     </h1>
 
+
     <blockquote>
         Sezione dedicata alle card dei progetti che visualizzerà il guest
     </blockquote>
+
 
     <p class="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor beatae officiis repellendus
         minima excepturi neque optio pariatur placeat amet quibusdam laborum autem, officia eos delectus quia nam molestiae?
