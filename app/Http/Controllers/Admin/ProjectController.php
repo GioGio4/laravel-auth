4ed49faafdf09dad6f1a5216791e964e11d63960
GioGio4 <g.ceccobelli@outlook.it>
2023-04-18 15:37:50 +0200

    Add delete project and destroy route

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -93,7 +93,9 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return to_route('admin.projects.index');
     }
 
     //   Validazione 
--- resources/views/Admin/projects/index.blade.php
+++ resources/views/Admin/projects/index.blade.php
@@ -27,7 +27,12 @@
                     <td>
                         <a href="{{ route('admin.projects.show', $project) }}"><i class="bi bi-eye-fill"></i></a>
                         <a href="{{ route('admin.projects.edit', $project) }}" class="action-icon"><i
-                                class="bi bi-pencil mx-2"></i></a>
+                                class="bi bi-pencil mx-2"></i>
+                        </a>
+                        <a type="button" class="text-danger" data-bs-toggle="modal"
+                            data-bs-target="#delete-modal-{{ $project->id }}">
+                            <i class="bi bi-trash mx-2"></i>
+                        </a>
                     </td>
                 </tr>
             @empty
@@ -35,4 +40,35 @@
         </tbody>
     </table>
     {{ $projects->links() }}
+
+    <!-- Modal -->
+    @foreach ($projects as $project)
+        <div class="modal fade" id="delete-modal-{{ $project->id }}" tabindex="-1"
+            aria-labelledby="delete-modal-{{ $project->id }}-label" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="delete-modal-{{ $project->id }}-label">
+                            <i class="bi bi-exclamation-triangle-fill me-3 text-danger"></i>Delete Project ID -
+                            {{ $project->id }}
+                        </h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body text-start">
+                        Are you sure you want to delete the project <strong>{{ $project->title }}</strong>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+
+                        <form action="{{ route('admin.projects.destroy', $project) }}" method="POST" class="">
+                            @method('DELETE')
+                            @csrf
+
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
 @endsection
